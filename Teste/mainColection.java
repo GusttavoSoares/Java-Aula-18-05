460eb14151e8b0a9d7be6be7d854d2ddf5693e0d
Gustavo Soares <63077870+GusttavoSoares@users.noreply.github.com>
2022-05-18 17:30:42 -0300

    Add files via upload

---
+++ Teste/mainColection.java
@@ -0,0 +1,21 @@
+public class mainColection {
+    public static void main (String[] args){
+        Colection vetores = new Colection();
+        vetores.printArrays();
+        vetores.printEquals();
+
+        int location = vetores.searchForInt(5);
+        if (location >= 0)
+            System.out.printf ("Encontrado elemento %d no Array!", location);
+        else
+        System.out.printf ("Não encontrado elemento no Array!");
+
+        location = vetores.searchForInt(8765);
+        if (location >= 0)
+        System.out.printf ("Encontrado elemento %d no Array!", location);
+        else
+        System.out.printf ("Não encontrado elemento no Array!");
+
+    }
+    
+}
